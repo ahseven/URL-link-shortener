0dcca62e8699b08cc1f694185e970c1e02ce0c2c
Rithw7 <146817425+ahseven@users.noreply.github.com>
2025-08-01 21:42:29 +0700

    Delete ndsf.py

--- ndsf.py
+++
@@ -1,4 +0,0 @@
-text1 = r"X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*"
-text2 = r"X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*"
-
-print(text1 == text2)  # This will print True if both strings are equal
